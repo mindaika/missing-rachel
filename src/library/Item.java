a34aa1adef2980418570bb6baba8294996fdaf83
Randall Sewell <randallsewell@gmail.com>
2013-11-08 11:42:40 -0800

    Initial commit

---
+++ src/library/Item.java
@@ -0,0 +1,9 @@
+// Each book, music album, or movie will be an instance of a subclass of this class.
+// Instances of this class should not be created. Specifying it as 'abstract' ensures
+// that they cannot.
+
+package library;
+
+public abstract class Item
+{
+}
